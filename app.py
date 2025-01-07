61ba3debf9e5467783f113e12fb0637027da1e0d
salsazufar <salsazufar@gmail.com>
2025-01-07 17:06:29 +0700

    fixing container width

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def main():
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+        st.image(image, caption='Uploaded Image', use_container_width=True)
         st.write("Classifying...")
 
         # Preprocess the image
